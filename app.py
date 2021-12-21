0e49df885f51a32521f3e3552bd76f615dd184ca
Dmitry <>
2021-12-21 08:50:02 +0200

    Started to do the Admin panel (as Django has). Started to add classes for the interview platform. Improved the appearance.

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
+# ----------------------------------------------------- IMPORT ---------------------------------------------------------
 from flask import Flask, request, redirect
 from flask import render_template  # Using for html templates
 from flask import url_for
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+# --------------------------------------------------- END IMPORT -------------------------------------------------------
+
+
+# ---------------------------------------------------- SETTINGS --------------------------------------------------------
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1@localhost/articles'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+# -------------------------------------------------- END SETTINGS ------------------------------------------------------
 
 
+# ----------------------------------------------------- CLASSES --------------------------------------------------------
 class Article(db.Model):
     __tablename__='articles'
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +34,45 @@ class Article(db.Model):
         return '<Article %r>' % self.id
 
 
+class User(db.Model):
+    column_display_pk = True
+    user_login = db.Column(db.String(200), primary_key=True)  # Need to do as a primary key
+    user_password = db.Column(db.String(200))
+    user_name = db.Column(db.String(200))
+    user_surname = db.Column(db.String(200), nullable=True)
+    user_type = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '<User %r>' % self.user_login
+
+
+
+# class User_interview(db.Model):
+#     pass
+#
+#
+# class Interview(db.Model):
+#     pass
+#
+#
+# class Interview_question(db.Model):
+#     pass
+#
+#
+# class User_interview_question(db.Model):
+#     pass
+#
+#
+# class Question(db.Model):
+#     pass
+#
+#
+# class Question_set(db.Model):
+#     pass
+# --------------------------------------------------- END CLASSES ------------------------------------------------------
+
+
+# ----------------------------------------------------- VIEWS ----------------------------------------------------------
 @app.route('/')  # '@' is a decorator. In brackets path to a specific page on a website.
 @app.route('/home')
 def index():
@@ -99,7 +149,17 @@ def create_article():
 # @app.route('/user/<string:name>/<int:id>')  # <string:name> is name of a unique user.
 # def user(name, id):
 #     return "User page: " + name + " - " + str(id)
+# --------------------------------------------------- END VIEWS --------------------------------------------------------
 
+
+# ----------------------------------------------------- ADMIN ----------------------------------------------------------
+app.config['FLASK_ADMIN_SWATCH'] = 'flatly'  # Set style
+admin = Admin(app, name='interview platform', template_mode='bootstrap3')
+
+admin.add_view(ModelView(Article, db.session))
+admin.add_view(ModelView(User, db.session))
+# --------------------------------------------------- END ADMIN --------------------------------------------------------
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.run(debug=True)
